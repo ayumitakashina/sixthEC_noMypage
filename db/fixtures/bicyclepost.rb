71a1fe4b35461f3ad60d218bbb3b53699f119c0a
AyumiTakashina <moira.horizon@gmail.com>
2017-07-27 02:32:59 +0000

    okcategory

--- db/fixtures/bicyclepost.rb
+++ db/fixtures/bicyclepost.rb
@@ -8,36 +8,63 @@ end
 
 Bicyclepost.seed do |s|
 	s.id = 2
+	s.name = "ビアンキ"
 	s.value = "120000"
 	s.bicyclecategory_id = 2
 end
 
 Bicyclepost.seed do |s|
 	s.id = 3
+	s.name = "パナソニック"
 	s.value = "150000"
 	s.bicyclecategory_id = 1
 end
 
 Bicyclepost.seed do |s|
 	s.id = 4
+	s.name = "ロードバイク"
 	s.value = "170000"
 	s.bicyclecategory_id = 1
 end
 
 Bicyclepost.seed do |s|
 	s.id = 5
+	s.name = "電動自転車"
 	s.value = "210000"
 	s.bicyclecategory_id = 1
 end
 
 Bicyclepost.seed do |s|
 	s.id = 6
+	s.name = "三輪車"
 	s.value = "260000"
 	s.bicyclecategory_id = 2
 end
 
 Bicyclepost.seed do |s|
 	s.id = 7
+	s.name = "チェーン"
 	s.value = "290000"
 	s.bicyclecategory_id = 1
+end
+
+Bicyclepost.seed do |s|
+	s.id = 8
+	s.name = "グリップ"
+	s.value = "3000"
+	s.bicyclecategory_id = 2
+end
+
+Bicyclepost.seed do |s|
+	s.id = 9
+	s.name = "チェーン用オイル"
+	s.value = "800"
+	s.bicyclecategory_id = 1
+end 
+
+Bicyclepost.seed do |s|
+	s.id = 10
+	s.name = "フロントライト"
+	s.value = "5000"
+	s.bicyclecategory_id = 2
 end
